3ef015b483a813a2d79e5374b50982bf18972618
George Livas <giorgos.livas.99@gmail.com>
2016-07-12 19:34:05 +0300

    code

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 for (let a = 3; 3 < 100; a++){
   console.log(a);
 }
+
+
+class Board {
+  constructor() {
+    this.board = [
+      [ null, null, null],
+      [ null, null, null],
+      [ null, null, null],
+    ];
+
+
+const X = "x";
+const O = "o";
+const prevPlayer;
+
+function firstPlay (x, o prev) {
+  if (prevPlayer === x) {
+    return 'O plays first';
+  } else {
+    return 'X plays first';
+  }
+
+}
+firstPlay(X, O, prevPlayer);
